f656d57c6643a31ffe6ce3f76ccfbef9fb22fd90
spopONFIRE <Fihdospop429@gmail.com>
2023-11-26 21:30:38 -0300

    exclusão de tarefas e finalização de projetos

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 //rotas
+app.get('/limparTarefas', (requisicao, resposta) => {
+    const sql = 'DELETE FROM tarefas'
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/excluir', (requisicao, resposta) => {
     const id = requisicao.body.id
 
